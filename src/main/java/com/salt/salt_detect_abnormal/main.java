36368406d62db2342fd19da4fc66a97e2d7c1d8e
Shirel <10shirel@gmail.com>
2023-02-27 18:29:30 +0200

    Build all maps

--- src/main/java/com/salt/salt_detect_abnormal/main.java
+++
@@ -1,9 +0,0 @@
-package com.salt.salt_detect_abnormal;
-
-public class main {
-    public static void main(final String[] args) {
-        for (int i = 1; i < 101; i++) {
-            System.out.println( ".נגה" + i);
-        }
-    }
-}
